543b29ac9bc5f5e8d175a4f541be03a12a94c5e7
WooYong <whdrmt12@gmail.com>
2017-07-05 10:57:04 +0530

    make 5 Activity

---
+++ app/src/main/java/india/lg/intern/fit/FootprintActivity.java
@@ -0,0 +1,13 @@
+package india.lg.intern.fit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FootprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_footprint);
+    }
+}
