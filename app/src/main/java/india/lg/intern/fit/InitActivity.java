9e6171493cb6c30403abefb92f940d959c803a3a
WooYong <whdrmt12@gmail.com>
2017-07-05 15:11:50 +0530

    Appbar 지우고 MainActivity 수정, 정보 클래스 만드는 중

--- app/src/main/java/india/lg/intern/fit/InitActivity.java
+++ app/src/main/java/india/lg/intern/fit/InitActivity.java
@@ -1,13 +1,23 @@
 package india.lg.intern.fit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class InitActivity extends AppCompatActivity {
+public class InitActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
+        ((Button) findViewById(R.id.button)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
---
+++ app/src/main/java/india/lg/intern/fit/Spot.java
@@ -0,0 +1,8 @@
+package india.lg.intern.fit;
+
+/**
+ * Created by WooYong on 2017-07-05.
+ */
+
+public class Spot {
+}
